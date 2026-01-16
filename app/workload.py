9aefde2e6eb6c3e77d817ca2184830242e48febf
anjish <40964308+anji5h@users.noreply.github.com>
2026-01-16 15:02:25 +0200

    updated workload config

--- app/workload.py
+++ app/workload.py
@@ -11,7 +11,7 @@ WORKLOAD_PROFILES: dict[WorkloadProfile, WorkloadConfig] = {
     WorkloadProfile.HIGH: WorkloadConfig(
         points_per_sec=1000,
         num_writers=3,
-        num_sensors=60000,
+        num_sensors=100000,
         min_pool_size=5,
         max_pool_size=15,
     ),
